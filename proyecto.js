e7d579fcd1a959695b72cc7a586c498d1d772254
DjCabe23 <damianramirez343@gmail.com>
2022-09-13 18:26:44 -0300

    Proyecto

--- proyecto.js
+++ proyecto.js
@@ -1,61 +1,77 @@
-let nombre = prompt("Ingrese nombre");
-entrada()
+let fecha = new Date();
+let año = fecha.getFullYear();
+let mes = fecha.getMonth() + 1;
+let dia = fecha.getDay();
+document.getElementById("reloj").innerHTML = dia + "/" + mes + "/" + año;
 
-function entrada() {
-    alert("Hola " + nombre + " que andaba buscando?")
-compra()
+class cliente {
+    informacion(nombre, apellido, email){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+    }
 }
 
-function compra() {
-    let objetos = Number(prompt("Elige una opcion: \n 1:Zapatillas \n 2:Remera \n 3:Pantalon \n 4:Pelota \n 5:Estas mirando nomas :)"))
-if (objetos =="1") { 
-    let elegir=Number(prompt(`La zapatillas salen $3000, desea llevarla?:\n 1:si \n 2:no`))
-    if(elegir == "1"){
-        metodoPago()
-    }else if (elegir == "2"){
-        compra()
-    }
-}else if(objetos =="2"){
-    let elegir= Number(prompt("La remera sale $6500, desea llevarla?: \n 1:si \n 2:no"))
-    if(elegir == "1"){
-        metodoPago()
-    }else if (elegir == "2"){
-        compra()
-    }    
-}else if(objetos =="3"){
-    let elegir= Number(prompt("El pantalon sale $8500, desea llevarla?: \n 1:si \n 2:no"))
-    if(elegir == "1"){
-        metodoPago()
-    }else if (elegir == "2"){
-        compra()
-    }
-}else if(objetos =="4"){
-    let elegir= Number(prompt("La pelota sale $2500, desea llevarla?: \n 1:si \n 2:no"))
-    if(elegir == "1"){
-        metodoPago()
-    }else if (elegir == "2"){
-        compra()
-    }
-}else if(objetos == "5"){    
-}else{
-    return compra()
-}
+let boton = document.getElementById("info");
+boton?.addEventListener("click", cargarInfo);
+
+function cargarInfo() {
+    let nombre = document.getElementById("Nombre").value;
+    let apellido = document.getElementById("Apellido").value;
+    let email = document.getElementById("Email").value;
+    let info1 = new cliente(nombre, apellido, email);
+    console.log(info1);
+    infoCliente(info1);
 }
 
-function metodoPago() {
-    let pago= Number(prompt("Con que desea pagar: \n 1:Efectivo \n 2:Tarjeta \n 3:me equivoque"))
-    if(pago == "1"){
-        alert(`Pagaste con efectivo. Muchas gracias ${nombre}`)
-    }else if(pago == "2"){
-        alert(`Pagaste con tarjeta. Muchas gracias ${nombre}`)
-    }else if(pago == "3"){
-        return compra()
-    }else if(pago != "1", "2", "3"){
-        alert("Por favor ingrese un numero")
-        return metodoPago()
-    }
-
+function infoCliente(cliente) {
+    let datos = document.getElementById("Informacion");
+    datos.innerHTML = "";
+    let nuevoContenedor = document.createElement("div");
+    nuevoContenedor.innerHTML = `Gracias ${cliente.nombre}, al correo ${cliente.email} le llegara la confirmacion de nueva cuenta, Gracias`; 
+    tipoDeProductos()
 }
+
+
+const contenedor = document.getElementById("ropaDeportiva");
+let tipoDeProductos = [
+    {id: 1, nombre: "Zapatilla", precio :2500, img : `./zapatilla.jpg`},
+    {id: 2, nombre: "Remera", precio :3000, img : `./remera.jpg`},
+    {id: 3, nombre: "Pantalon", precio :4500, img : `./pantalom.jpg`},
+    ]
+
+tipoDeProductos.forEach((producto) =>{
+    const div = document.createElement(`div`);
+    div.classList.add("producto");
+    div.innerHTML = `
+    <img src=${producto.img}>
+    <h4>${producto.nombre}</h4>
+    <p>${producto.precio}</p>
+    <button id ="agregar${producto.id}" class="boton-agregar>AGREGAR</button>"`
+
+    contenedor.appendChild(div)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
